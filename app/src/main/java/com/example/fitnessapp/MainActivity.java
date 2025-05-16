8da164324400f18ddb399b30dfb15987662944d1
firte14 <favourajiduah@gmail.com>
2025-05-16 15:59:48 +0100

    .

--- app/src/main/java/com/example/fitnessapp/MainActivity.java
+++ app/src/main/java/com/example/fitnessapp/MainActivity.java
@@ -5,15 +5,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
-import java.util.Timer;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Button timerScreen;
